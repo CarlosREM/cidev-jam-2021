b2b5874d13e8bbbd98cb7f9981a2ca65ad90f115
Carlos Esquivel <carlosrem70@gmail.com>
2021-06-26 12:49:48 -0600

    Menu anims, Scene Transition, Music Manager

---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TransitionManager : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] float transitionDuration;
+
+    AudioManager audioManager;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start() {
+        audioManager = GameObject.Find("Music Manager").GetComponent<AudioManager>();
+        audioManager.FadeAudio(transitionDuration, 0, 1);
+    }
+
+    public void ChangeScene(string sceneName) {
+        Debug.Log("TransitionManager > Loading Scene \""+sceneName+"\"");
+
+        StartCoroutine(SceneSwitchCoroutine(sceneName));
+    }
+
+    IEnumerator SceneSwitchCoroutine(string sceneName) {
+        animator.SetTrigger("Transition");
+        audioManager.FadeAudio(transitionDuration, 1, 0);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    bool AnimatorIsPlaying() {
+        return animator.GetCurrentAnimatorStateInfo(0).length >
+                animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+}
